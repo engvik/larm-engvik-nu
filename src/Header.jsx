98c0ff014cb62da0ea46fa1db4e9ef7f5c674024
Lars Jansøn Engvik <lars@engvik.nu>
2018-02-13 21:09:45 +0100

    Be lazy and copy 2017

---
+++ src/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div className="header">
+        <h1>Free:Larm</h1>
+        <p>01.03 - 04.03</p>
+        <p>En oversikt over gratiskonsertene by:Larm-uka.</p>
+      </div>
+    );
+  }
+}
+
+export default Header;
---
+++ src/concert-data.js
@@ -0,0 +1,58 @@
+/** Skeleton:
+
+{
+  day: "",
+  venues: [
+    {
+      title: "",
+      url: "",
+      venue: "",
+      info: "",
+      concerts: [
+        {
+          name: "",
+          time: ""
+        },
+        {
+          name: "",
+          time: ""
+        },
+        {
+          name: "",
+          time: ""
+        },
+      ]
+    }
+  ]
+}
+
+*/
+
+const concertData = [
+  {
+    day: "Onsdag",
+    venues: [
+      
+    ]
+  },
+  {
+    day: "Torsdag",
+    venues: [
+      
+    ]
+  },
+  {
+    day: "Fredag",
+    venues: [
+      
+    ]
+  },
+  {
+    day: "Lørdag",
+    venues: [
+     
+    ]
+  }
+]
+
+export default concertData;
