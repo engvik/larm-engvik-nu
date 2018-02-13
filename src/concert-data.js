c4217176de115dff52a6fd5dccd3dea4651517ab
Lars Jansøn Engvik <lars@engvik.nu>
2018-02-13 21:23:53 +0100

    Add some data

--- src/concert-data.js
+++ src/concert-data.js
@@ -30,27 +30,84 @@
 
 const concertData = [
   {
-    day: "Onsdag",
-    venues: [
-      
-    ]
-  },
-  {
-    day: "Torsdag",
-    venues: [
-      
-    ]
-  },
-  {
-    day: "Fredag",
-    venues: [
-      
-    ]
-  },
-  {
     day: "Lørdag",
     venues: [
-     
+      {
+        title: "Apollon Records presenterer off:Larm",
+        url: "https://www.facebook.com/events/142193899782039/",
+        venue: "Uhørt",
+        info: "",
+        concerts: [
+          {
+            name: "Simen Lyngroth",
+            time: "16:00"
+          },
+          {
+            name: "I am K",
+            time: "16:45"
+          },
+          {
+            name: "Major Parkinson",
+            time: "17:20"
+          },
+          {
+            name: "UNGDOMSKULEN",
+            time: "18:10"
+          },
+          {
+            name: "Borgar Storebråten",
+            time: "18:50"
+          },
+          {
+            name: "Slomosa",
+            time: "19:30"
+          }
+        ]
+      },
+      {
+        title: "Brylarm",
+        url: "https://www.facebook.com/events/1888393454804748/",
+        venue: "Kulturkirken JAKOB",
+        info: "",
+        concerts: [
+          {
+            name: "deLillos",
+            time: ""
+          },
+          {
+            name: "Skambankt",
+            time: ""
+          },
+          {
+            name: "Marion Ravn",
+            time: ""
+          },
+          {
+            name: "Hilma Nikolaisen",
+            time: ""
+          },
+          {
+            name: "Oslo Ess",
+            time: ""
+          }
+        ]
+      },
+      {
+        title: "Vise:larm",
+        url: "https://www.facebook.com/events/357639181370982/",
+        venue: "Café Mir",
+        info: "Usikkert om dette er gratis.",
+        concerts: [
+          {
+            name: "Emma Hegermann",
+            time: ""
+          },
+          {
+            name: "Bøler",
+            time: ""
+          }
+        ]
+      }
     ]
   }
 ]
