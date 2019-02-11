44b56d0f4700dc293af8592dd7f24bf5b9679600
Lars Jansøn Engvik <lars@engvik.nu>
2019-02-11 21:07:43 +0100

    Add more shows (#7)

--- src/app/state.cljs
+++ src/app/state.cljs
@@ -10,10 +10,19 @@
                                    {:name "Birgitta Alida" :time ""}
                                    {:name "Pelicat" :time ""}
                                    {:name "Verdensrommet DJs" :time ""}]}
-                      {:title "Furuberget Showcase" :venue "Tilt" :url "https://www.facebook.com/events/381909052634802/" :info ""
+                       {:title "Furuberget Showcase" :venue "Tilt" :url "https://www.facebook.com/events/381909052634802/" :info ""
                         :concerts [{:name "Wild Fauna" :time ""}
                                    {:name "TAPE TRASH" :time ""}]}]}
-             {:day "Torsdag"}
+             {:day "Torsdag"
+                        :venues [{:title "Jansen Records/Pekula/Blanca Records" :venue "Tilt" :url "https://www.facebook.com/events/2296681343698920/" :info ""
+                        :concerts [{:name "Kongle" :time "19:00"}
+                                   {:name "Birgitta Alida" :time "20:00"}
+                                   {:name "TBA (US/NO)" :time "20:30"}
+                                   {:name "Arthur Kay" :time "21:00"}
+                                   {:name "Louien" :time "21:30"}
+                                   {:name "Orions Belte" :time "22:00"}
+                                   {:name "TBA" :time "23:00"}
+                                   {:name "Amgala Temple" :time "00:00"}]}]}
              {:day "Fredag"
               :venues [{:title "Apollon Records Off:Larm Showcase" :venue "Uhørt" :url "https://www.facebook.com/events/244316283123036/" :info ""
                         :concerts [{:name "Deathcrush" :time ""}
@@ -21,11 +30,15 @@
                                    {:name "Subshine" :time ""}
                                    {:name "The Tronosonic Experience" :time ""}
                                    {:name "35 TAPES" :time ""}]}
-                        {:title "Mother Likes It Showcase" :venue "Tilt" :url "https://www.facebook.com/events/2174182142843786/" :info ""
+                       {:title "Mother Likes It Showcase" :venue "Tilt" :url "https://www.facebook.com/events/2174182142843786/" :info ""
                         :concerts [{:name "Magnus Hængsle" :time "19:00"}
                                    {:name "Ludvig The Band" :time "20:00"}
                                    {:name "Nellys Crush" :time "21:00"}
-                                   {:name "Brut Booglaoo" :time "22:00"}]}]}
+                                   {:name "Brut Booglaoo" :time "22:00"}]}
+                       {:title "Loyal Blood Records Off:larm labelnight" :venue "Vaterland" :url "https://www.facebook.com/events/487810885083685/" :info ""
+                        :concerts [{:name "Death Throne" :time ""}
+                                   {:name "Mansters" :time ""}
+                                   {:name "Psykopath" :time ""}]}]}
              {:day "Lørdag"
               :venues [{:title "Brylarm" :venue "Kulturkirken Jakob" :url "https://www.facebook.com/events/300541287479380/" :info ""
                         :concerts [{:name "Oslo Ess" :time ""}
