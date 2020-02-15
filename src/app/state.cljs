94dc3bd596fb8e432fba5a612802d0895a265ee7
Lars Jansøn Engvik <lars@engvik.nu>
2020-02-15 14:41:09 +0100

    feature(2020): vaterland

--- src/app/state.cljs
+++ src/app/state.cljs
@@ -16,7 +16,11 @@
                                      {:name "Ljushuvud" :time "18:50"}
                                      {:name "Jag & Ellen" :time "19:40"}
                                      {:name "Killer Kid Mozart" :time "20:30"}
-                                     {:name "BRAINCOATS" :time "21:20"}]}]}
+                                     {:name "BRAINCOATS" :time "21:20"}]}
+                        {:title "Vaterlarm" :venue "Vaterland" :url "https://www.facebook.com/events/783597285470547/" :info ""
+                          :concerts [{:name "Blackwater" :time "21:00"}
+                                     {:name "Lazy Queen" :time "22:00"}
+                                     {:name "WIZRD" :time "23:00"}]}]}
              {:day "Fredag"
               :venues [{:title "SellOut!/Mother Likes It/Sheep Chase Showcase" :venue "Tilt" :url "https://www.facebook.com/events/470698936954829/" :info ""
                         :concerts [{:name "Tone Bringsdal" :time ""}
@@ -38,7 +42,11 @@
                         :concerts [{:name "Jørund Vålandsmyr & Menigheten" :time "19:30"}
                                    {:name "TBA" :time "20:30"}
                                    {:name "The MallRats." :time "21:30"}
-                                   {:name "Carina Frantzen" :time "22:30"}]}]}
+                                   {:name "Carina Frantzen" :time "22:30"}]}
+                       {:title "Vaterlarm" :venue "Vaterland" :url "https://www.facebook.com/events/206093270509856/" :info ""
+                          :concerts [{:name "Rackstraw" :time "21:00"}
+                                     {:name "Third Stone" :time "22:00"}
+                                     {:name "Draken" :time "23:00"}]}]}
              {:day "Lørdag"
               :venues [{:title "Fysisk Format Showcase" :venue "Tilt" :url "https://www.facebook.com/events/850496845406794/" :info ""
                         :concerts [{:name "HYMN" :time ""}
@@ -47,25 +55,29 @@
                                    {:name "Ormskrik" :time ""}
                                    {:name "IEatHeartAttacks" :time ""}
                                    {:name "The Secret Sound of Dreamwalkers" :time ""}]}
-                      {:title "Torggatafest" :venue "LOKK" :url "https://bylarm.no/music/news/torggatafest/" :info ""
+                       {:title "Vaterlarm" :venue "Vaterland" :url "https://www.facebook.com/events/186716042737025/" :info ""
+                          :concerts [{:name "MAKTKAMP" :time "21:00"}
+                                     {:name "Kennel" :time "22:00"}
+                                     {:name "Anti Social Rejects" :time "23:00"}]}
+                       {:title "Torggatafest" :venue "LOKK" :url "https://bylarm.no/music/news/torggatafest/" :info ""
                         :concerts [{:name "Jonathan Floyd" :time "15:00"}
                                    {:name "UngeKatter" :time "15:40"}
                                    {:name "J-Zino" :time "16:20"}
                                    {:name "Aram" :time "17:00"}
                                    {:name "Bård Berg" :time "17:40"}]}
-                      {:title "Torggatafest" :venue "Vans" :url "https://bylarm.no/music/news/torggatafest/" :info ""
+                       {:title "Torggatafest" :venue "Vans" :url "https://bylarm.no/music/news/torggatafest/" :info ""
                         :concerts [{:name "SANDSLOTT" :time "14:30"}
                                    {:name "Doglover95" :time "15:20"}
                                    {:name "Helmer" :time "16:00"}
                                    {:name "Tapewind" :time "16:40"}
                                    {:name "A Different Message" :time "17:20"}]}
-                      {:title "Torggatafest" :venue "Vintage Gitar" :url "https://bylarm.no/music/news/torggatafest/" :info ""
+                       {:title "Torggatafest" :venue "Vintage Gitar" :url "https://bylarm.no/music/news/torggatafest/" :info ""
                         :concerts [{:name "von werden" :time "14:00"}
                                    {:name "KitFai" :time "14:40"}
                                    {:name "Castaway Hounds" :time "15:20"}
                                    {:name "WIZRD" :time "16:00"}
                                    {:name "wingdings" :time "16:40"}]}
-                      {:title "Torggatafest" :venue "Oche" :url "https://bylarm.no/music/news/torggatafest/" :info ""
+                       {:title "Torggatafest" :venue "Oche" :url "https://bylarm.no/music/news/torggatafest/" :info ""
                         :concerts [{:name "Caterina Eliza" :time "13:00"}
                                    {:name "Benedikt" :time "13:40"}
                                    {:name "AWSE" :time "14:20"}
@@ -73,7 +85,7 @@
                                    {:name "Zueva" :time "15:40"}
                                    {:name "INGO" :time "16:20"}
                                    {:name "I See Rivers" :time "17:00"}]}
-                      {:title "Torggatafest" :venue "Festivalteltet" :url "https://bylarm.no/music/news/torggatafest/" :info ""
+                       {:title "Torggatafest" :venue "Festivalteltet" :url "https://bylarm.no/music/news/torggatafest/" :info ""
                         :concerts [{:name "Banaan" :time "15:00"}
                                    {:name "Anna Soleil" :time "15:40"}
                                    {:name "Louam" :time "16:20"}
